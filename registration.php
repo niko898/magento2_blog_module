ae7013c4a17ad620ae6dda2e47b1df659d561ac7
Mykola Akimov <wildniko88@gmail.com>
2023-09-04 16:28:47 +0400

    module created and database

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE, 
+    'MykolaAkimov_SimpleBlog',
+     __DIR__);
