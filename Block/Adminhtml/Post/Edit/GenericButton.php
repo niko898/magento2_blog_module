7071d70c3bf7aa938620762e5c741b5f769bc52b
Mykola Akimov <wildniko88@gmail.com>
2023-09-05 14:23:51 +0400

    added buttons to form

---
+++ Block/Adminhtml/Post/Edit/GenericButton.php
@@ -0,0 +1,18 @@
+<?php
+declare(strict_types=1);
+
+namespace MykolaAkimov\SimpleBlog\Block\Adminhtml\Post\Edit;
+
+use Magento\Framework\UrlInterface;
+
+class GenericButton
+{
+    public function __construct(
+        private UrlInterface $url
+    ){}
+  
+    public function getUrl(string $route, array $params = []): string 
+    {
+        return $this->url->getUrl($route, $params);
+    }
+}
